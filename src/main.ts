4dee89feb33911891b70d738d9e8c8986dfe64e1
DenerBorges <denerkborges@bol.com.br>
2023-10-06 14:46:23 -0300

    feat: enabling cors to two urls

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://stimulus.fly.dev'],
   });
 
   app.setGlobalPrefix('/api/v1/');
