95b0bc5b84b35e4b44006f4cb7f7e03c6142b91e
DenerBorges <denerkborges@bol.com.br>
2024-03-14 19:02:39 -0300

    build: installing cors npm install @types/cors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
+  const corsOptions = {
     origin: [
-      'http://localhost:3000',
-      'https://stimulus-tcc-fe.vercel.app',
-      'https://api.mercadopago.com/v1/',
+      'http://localhost:3000', // Permitir acesso local para desenvolvimento
+      'https://stimulus-tcc-fe.vercel.app', // Permitir acesso ao site do Vercel
     ],
-    preflightContinue: false,
-  });
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization', // Cabeçalhos permitidos
+    credentials: true, // Permitir envio de credenciais (cookies, cabeçalhos de autorização)
+    optionsSuccessStatus: 200, // Status de sucesso para solicitações OPTIONS
+  };
+
+  app.use(cors(corsOptions));
 
   app.setGlobalPrefix('/api/v1/');
 
