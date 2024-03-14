db11ad5cba063c66cd3abc6147a02b758a0a8b4c
DenerBorges <denerkborges@bol.com.br>
2024-03-14 19:26:56 -0300

    fix: enable cors issue

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const corsOptions = {
+  app.enableCors({
     origin: [
-      'http://localhost:3000', // Permitir acesso local para desenvolvimento
-      'https://stimulus-tcc-fe.vercel.app', // Permitir acesso ao site do Vercel
+      'http://localhost:3000',
+      'https://stimulus-tcc-fe.vercel.app',
+      'https://api.mercadopago.com/v1/',
     ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
-    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization', // Cabeçalhos permitidos
-    credentials: true, // Permitir envio de credenciais (cookies, cabeçalhos de autorização)
-    optionsSuccessStatus: 200, // Status de sucesso para solicitações OPTIONS
-  };
-
-  app.use(cors(corsOptions));
+  });
 
   app.setGlobalPrefix('/api/v1/');
 
