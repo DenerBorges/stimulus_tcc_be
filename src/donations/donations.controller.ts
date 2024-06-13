336217ac00047b6a23a4909df02e91eb11dbc70d
DenerBorges <denerkborges@bol.com.br>
2024-06-13 17:18:14 -0300

    feat: showing all the donations without being loged in

--- src/donations/donations.controller.ts
+++ src/donations/donations.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { DonationsService } from './donations.service';
 import { Donation } from '@prisma/client';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('donations')
 export class DonationsController {
@@ -11,6 +12,7 @@ export class DonationsController {
     return this.donationsService.create(donation);
   }
 
+  @IsPublic()
   @Get()
   findAll(): Promise<Donation[]> {
     return this.donationsService.findAll();
